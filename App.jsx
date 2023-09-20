8cc02ff81d42093d239b93aaed2b58defe023f9d
marine-spaak <marine.spaak@gmail.com>
2023-09-20 17:30:20 +0200

    basic notification with toastify

--- App.jsx
+++ App.jsx
@@ -1,16 +1,24 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import ToastManager, { Toast } from 'toastify-react-native';
 
 import Home from './src/components/Home/Home';
 import { Header } from './src/components';
 import style from './App.style';
 
-const App = () => (
-  <SafeAreaProvider>
-    <SafeAreaView style={style.appContainer}>
-      <Header />
-      <Home />
-    </SafeAreaView>
-  </SafeAreaProvider>
-);
+const App = () => {
+  const showToasts = () => {
+    Toast.success('Promised is resolved');
+  };
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={style.appContainer}>
+        <ToastManager />
+        <Header />
+        <Home />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
 
 export default App;
